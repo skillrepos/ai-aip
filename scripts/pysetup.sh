48d655f3177371d3f225432db7455a1b8265908f
Brent Laster <bclaster@nclasters.org>
2026-01-04 21:51:28 -0500

    Add fallback for requirements installation

--- scripts/pysetup.sh
+++ scripts/pysetup.sh
@@ -10,6 +10,8 @@ source ./$PYTHON_ENV/bin/activate
 
 if [ -f "./requirements.txt" ]; then
   pip3 install -r "./requirements.txt"
+elif [ -f "./requirements/requirements.txt" ]; then
+  pip3 install -r "./requirements/requirements.txt"
 else
   pip3 install -r "/workspaces/ai-aip/requirements/requirements.txt"
 fi
